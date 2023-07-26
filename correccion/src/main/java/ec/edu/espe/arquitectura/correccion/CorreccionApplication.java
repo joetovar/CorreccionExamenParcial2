8f0cc38ddda5994b4361b127c8381db91f8416c4
Jerry Tovar <65546801+joetovar@users.noreply.github.com>
2023-07-26 10:49:37 -0500

    update correccion

---
+++ correccion/src/main/java/ec/edu/espe/arquitectura/correccion/CorreccionApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.correccion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorreccionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorreccionApplication.class, args);
+	}
+
+}
---
+++ correccion/src/test/java/ec/edu/espe/arquitectura/correccion/CorreccionApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.correccion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorreccionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
