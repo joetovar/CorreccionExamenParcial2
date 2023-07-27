dab6cded8d4b7f2600e9a14ccb4ad5a605499481
Jerry Tovar <65546801+joetovar@users.noreply.github.com>
2023-07-27 12:58:53 -0500

    update Dto

---
+++ correccion/src/main/java/ec/edu/espe/arquitectura/correccion/dto/empleadoDto.java
@@ -0,0 +1,41 @@
+package ec.edu.espe.arquitectura.correccion.dto;
+
+public class empleadoDto {
+    private String cedulaIdentidad;
+    private String apellidos;
+    private String nombres;
+    private String numeroCuenta;
+    public empleadoDto() {
+    }
+    public String getCedulaIdentidad() {
+        return cedulaIdentidad;
+    }
+    public void setCedulaIdentidad(String cedulaIdentidad) {
+        this.cedulaIdentidad = cedulaIdentidad;
+    }
+    public String getApellidos() {
+        return apellidos;
+    }
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    public String getNombres() {
+        return nombres;
+    }
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+    public empleadoDto(String cedulaIdentidad, String apellidos, String nombres, String numeroCuenta) {
+        this.cedulaIdentidad = cedulaIdentidad;
+        this.apellidos = apellidos;
+        this.nombres = nombres;
+        this.numeroCuenta = numeroCuenta;
+    }
+
+}
---
+++ correccion/src/main/java/ec/edu/espe/arquitectura/correccion/dto/empleadoPagoDto.java
@@ -0,0 +1,28 @@
+package ec.edu.espe.arquitectura.correccion.dto;
+
+import java.math.BigDecimal;
+
+public class empleadoPagoDto {
+
+    private String numeroCuenta;
+    private BigDecimal valor;
+    public empleadoPagoDto() {
+    }
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+    public BigDecimal getValor() {
+        return valor;
+    }
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+    public empleadoPagoDto(String numeroCuenta, BigDecimal valor) {
+        this.numeroCuenta = numeroCuenta;
+        this.valor = valor;
+    }
+
+}
