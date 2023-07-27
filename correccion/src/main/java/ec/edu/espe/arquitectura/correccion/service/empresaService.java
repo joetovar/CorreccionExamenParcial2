59d6c6901c3e8c8bef1ca44497218209214b92e9
Jerry Tovar <65546801+joetovar@users.noreply.github.com>
2023-07-27 12:52:10 -0500

    update Services

---
+++ correccion/src/main/java/ec/edu/espe/arquitectura/correccion/service/empresaService.java
@@ -0,0 +1,34 @@
+package ec.edu.espe.arquitectura.correccion.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ec.edu.espe.arquitectura.correccion.dto.empresaDto;
+import ec.edu.espe.arquitectura.correccion.model.empresa;
+import ec.edu.espe.arquitectura.correccion.repository.empresaRepository;
+
+
+@Service
+public class empresaService {
+    @Autowired
+    empresaRepository empresaRepository;
+
+    
+    public empresaService(empresaRepository empresaRepository) {
+        this.empresaRepository = empresaRepository;
+    }
+
+    public empresa crearEmpresaConEmpleados(empresaDto empresaDTO) {
+        empresa empresa = EmpresaDtoToEmpresa(empresaDTO);
+        return empresaRepository.save(empresa);
+    }
+
+    private empresa EmpresaDtoToEmpresa(empresaDto empresaDTO) {
+        empresa empresa = new empresa();
+        empresa.setRUC(empresaDTO.getRUC());
+        empresa.setRazonSocial(empresaDTO.getRazonSocial());
+        empresa.setCuentaPrincipal(empresaDTO.getCuentaPrincipal());
+        empresa.setEmpleados(empresaDTO.getEmpleados());
+        return empresa;
+    }
+}
